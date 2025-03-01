d738b89f98012d73b20b18bc9bb0f47d7c2a6302
kartik-soni1707 <its.kartiksoni@gmail.com>
2025-03-01 13:54:18 -0800

    added tech field

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,6 +7,7 @@ interface Project {
   image: string;
   link: string;
   details: string;
+  tech: string[],
 }
 @Component({
   selector: 'app-projects',
@@ -26,6 +27,7 @@ export class ProjectsComponent {
       image: 'assets/mywebsitr.png',
       link: 'https://kartiksoni1707.wixsite.com/creativity',
       details: 'This is a portfolio website showcasing my projects and skills using Angular and modern web technologies. It features a dynamic project grid, responsive design, and an interactive UI.',
+      tech:["JS"],
     },
     {
       title: 'Mask Detection',
@@ -33,6 +35,7 @@ export class ProjectsComponent {
       image: 'assets/mask_detection.png',
       link: 'https://github.com/yourrepo/mask-detection',
       details: 'A research-based project that leverages deep learning and computer vision techniques to detect whether a person is wearing a mask in real-time images.',
+      tech:[],
     },
     {
       title: 'Event Management Platform',
@@ -40,6 +43,7 @@ export class ProjectsComponent {
       image: 'assets/event_management.png',
       link: 'https://whova.com',
       details: 'Built scalable microservices and backend APIs for an event management system, enhancing user engagement and platform efficiency. Worked on authentication, event scheduling, and data analytics features.',
+      tech:[],
     }
   ];
   
