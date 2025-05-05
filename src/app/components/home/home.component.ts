4e4c1b034b7c7a8aeef505b407f8cb72e83809bf
kartik-soni1707 <its.kartiksoni@gmail.com>
2025-05-05 20:34:58 +0530

    decluttered stuff

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,16 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  intro ="👋 Hello! I’m Kartik Soni, a passionate software engineer with a strong foundation in computer science and a keen interest in artificial intelligence and machine learning. With a Bachelor of Science in Computer Science from the University of Massachusetts Amherst, I have developed a diverse skill set that includes full-stack web development, machine learning, and cloud technologies. My experience at Whova, coupled with my research in image processing and machine learning, has equipped me to tackle complex challenges and contribute innovative solutions. I thrive on continuous learning and collaboration, aiming to make a meaningful impact in the tech industry.";
+  items = ["👋 Hello! I’m Kartik Soni, a passionate software engineer with a strong foundation in computer science and a keen interest in artificial intelligence and machine learning.\n" ,"With a Bachelor of Science in Computer Science from the University of Massachusetts Amherst, I have developed a diverse skill set that includes full-stack web development, machine learning, and cloud technologies.\n",
+  "My experience at Whova, coupled with my research in image processing and machine learning, has equipped me to tackle complex challenges and contribute innovative solutions.\n",
+  "I thrive on continuous learning and collaboration, aiming to make a meaningful impact in the tech industry."];
+  
+
 }
