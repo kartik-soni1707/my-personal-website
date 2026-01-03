4de6831be65b2e826d71e8cc7a820fa3c5859e84
kartik-soni1707 <its.kartiksoni@gmail.com>
2026-01-02 18:09:04 -0800

    routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { loggingInterceptor } from './service/logger.service';
-import { routes } from './app.routes';
+import { routes } from './routing/app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([loggingInterceptor]))]
--- src/app/routing/app.routes.ts
+++ src/app/routing/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { ProjectsComponent } from './components/projects/projects.component';
+import { HomeComponent } from '../components/home/home.component';
+import { AboutComponent } from '../components/about/about.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
 
 export const routes: Routes = 
 [ { path: '', component: HomeComponent }, // Maps "/" to HomeComponent
